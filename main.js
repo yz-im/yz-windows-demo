bd9aae92aaa67297905c923c72e4cb1a1becfd29
DESKTOP-POH3U2O\terry <terrychaoc@gmail.com>
2023-12-31 22:42:06 +0800

    feat: add react

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev')
+const path = require('path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 400,
         height: 800
     })
-
-    win.loadFile('index.html')
+    if (isDev) {
+        win.loadURL('http://localhost:3000')
+    } else {
+        win.loadFile(path.join(__dirname, '../build/index.html'))
+    }
 }
 
 app.whenReady().then(() => {
